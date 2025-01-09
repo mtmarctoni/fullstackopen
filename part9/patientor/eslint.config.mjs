ec340e9b210751f4d1759039c7878214b066f090
mtmarctoni <marctonimas@protonmail.com>
2025-01-09 12:42:07 +0100

    part9 exercises 9.8-9.9

---
+++ part9/patientor/eslint.config.mjs
@@ -0,0 +1,41 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+          parserOptions: {
+            project: true,
+            tsconfigRootDir: import.meta.dirname,
+          },
+          parser: tseslint.parser,
+        },
+        plugins: {
+            "@stylistic": stylistic,
+        },
+        rules: {
+          '@stylistic/semi': 'error',
+          '@typescript-eslint/no-unsafe-assignment': 'error',
+          '@typescript-eslint/no-explicit-any': 'error',
+          '@typescript-eslint/explicit-function-return-type': 'off',
+          '@typescript-eslint/explicit-module-boundary-types': 'off',
+          '@typescript-eslint/restrict-template-expressions': 'off',
+          '@typescript-eslint/restrict-plus-operands': 'off',
+          '@typescript-eslint/no-unused-vars': [
+            'error',
+            { 'argsIgnorePattern': '^_' }
+          ],
+          'no-case-declarations': 'off',
+        },
+    },
+    {
+        ignores: ["build/**/*"]
+    }
+);
